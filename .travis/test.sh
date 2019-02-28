96ff4b3ba1e534e7aa1631aba1f7db909dd9ccb2
Evgeni Golov <evgeni@golov.de>
2019-02-28 14:46:54 +0100

    force en_US.UTF-8 on travis and run tests on centos

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,4 +1,16 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
+
+export LC_ALL=en_US.UTF-8
+
+if [ -x /usr/bin/yum ]; then
+  yum install -y docbook-utils openssl rpm-build
+fi
+
+docbook2man katello-ssl-tool.sgml
+
+python setup.py install
 
 for i in `seq 1 4`; do
 katello-ssl-tool --gen-ca -p file:/etc/pki/katello/private/katello-default-ca.pwd --force --ca-cert-dir /etc/pki/katello-certs-tools/certs --set-common-name example.com --ca-cert katello-default-ca.crt --ca-key katello-default-ca.key --ca-cert-rpm katello-default-ca --set-country US --set-state "North Carolina" --set-city Raleigh --set-org Katello --set-org-unit SomeOrgUnit
